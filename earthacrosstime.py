da842063eb350358ed7ec329693a343e3739c224
Noah Doersing <git@noahdoersing.com>
2020-10-09 21:04:38 +0200

    Progress

--- earthacrosstime.py
+++ earthacrosstime.py
@@ -9,12 +9,12 @@ from moviepy.editor import *
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-M = None  # global for metadata
-
 class Config:
     def __init__(self, config):
         self.temp_dir = config['GENERAL']['temp_dir']
         self.timemachine_repository_url = config['TIMEMACHINE']['timemachine_repository_url']
+        if (self.timemachine_repository_url[-1] != "/"):
+            self.timemachine_repository_url += "/"
 
 class Metadata:
     def __init__(self, timemachine_repository_url, dataset, projection_bounds, capture_times, frames, fps, level_info, nlevels, width, height, tile_width, tile_height, video_width, video_height):
@@ -172,6 +172,7 @@ class ZoomLevel:
         self.meters_per_pixel = meters_per_pixel
 
 # rel to width, height
+# TODO rename
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -184,52 +185,56 @@ class Tile:
         self.row = row
 
     # TODO attribute this snippet: based on ...
-    # TODO could move this as a init method into tile class, with static attributes for M.*
     @classmethod
-    def from_point_and_level(cls, point, level):
-        level_scale = math.pow(2, M.nlevels - 1 - level.level)
+    def from_point_and_level(cls, point, level, metadata):
+        level_scale = math.pow(2, metadata.nlevels - 1 - level.level)
 
-        col = round((point.x - (M.video_width * level_scale * 0.5)) / (M.tile_width * level_scale))
+        col = round((point.x - (metadata.video_width * level_scale * 0.5)) / (metadata.tile_width * level_scale))
         col = max(col, 0)
-        col = min(col, M.level_info[level.level]['cols'] - 1)
+        col = min(col, metadata.level_info[level.level]['cols'] - 1)
 
-        row = round((point.y - (M.video_height * level_scale * 0.5)) / (M.tile_height * level_scale))
+        row = round((point.y - (metadata.video_height * level_scale * 0.5)) / (metadata.tile_height * level_scale))
         row = max(row, 0)
-        row = min(row, M.level_info[level.level]['rows'] - 1)
+        row = min(row, metadata.level_info[level.level]['rows'] - 1)
 
         return cls(level, col, row)
 
-# TODO split into timelapsevideo and videoprocessor/finishing/processing that would get a timelapsevideo and some metadata?
-# TODO assertions at top of process() could go into verify method?
-class TimelapseVideo:
-    def __init__(self, tile, temp_dir):
+class RawVideo:
+    def __init__(self, tile, base_url, temp_dir):
         self.tile = tile
+        self.base_url = base_url
         self.temp_dir = temp_dir
-        base_filename = f"earthacrosstime-{tile.level.level}-{tile.row}-{tile.col}"
-        self.raw = os.path.join(self.temp_dir, base_filename + "-raw.mp4")
-        self.processed = os.path.join(self.temp_dir, base_filename + "-processed.mp4")
+
+        self.url = f"{base_url}/{tile.level.level}/{tile.row}/{tile.col}.mp4"
+        self.path = os.path.join(temp_dir, f"{tile.level.level}-{tile.row}-{tile.col}-raw.mp4")
 
     def __write_to_temp(self, video_data):
         if not os.path.isdir(self.temp_dir):
             os.makedirs(self.temp_dir)
-        with open(self.raw, 'wb') as f:
+        with open(self.path, 'wb') as f:
             f.write(video_data)
 
     def download(self):
-        url = f"{M.timemachine_repository_url}{M.dataset}/{self.tile.level.level}/{self.tile.row}/{self.tile.col}.mp4"
-
-        r = requests.get(url)
+        r = requests.get(self.url)
         if r.status_code != 200:
-            raise ValueError(f"Unable to download tile from {url}, status code {r.status_code}.")
+            raise ValueError(f"Unable to download tile from {self.url}, status code {r.status_code}.")
 
         self.__write_to_temp(r.content)
 
-    def verify(self):
-        clip = VideoFileClip(self.raw)
-        assert clip.w == M.video_width
-        assert clip.h == M.video_height
-        assert clip.fps == M.fps
-        assert clip.duration == M.frames / M.fps
+    def check_against(self, metadata):
+        clip = VideoFileClip(self.path)
+        assert clip.w == metadata.video_width
+        assert clip.h == metadata.video_height
+        assert clip.fps == metadata.fps
+        assert clip.duration == metadata.frames / metadata.fps
+
+class VideoEditor:
+    def __init__(self, video, geopoint, capture_times):
+        self.video = video
+        self.geopoint = geopoint
+        self.capture_times = capture_times
+
+        self.path = os.path.join(video.temp_dir, f"{video.tile.level.level}-{video.tile.row}-{video.tile.col}-processed.mp4")
 
     def __draw_text(self, text, fontsize):
         fnt = ImageFont.truetype("Optician-Sans.otf", fontsize)  # TODO configurable
@@ -238,7 +243,6 @@ class TimelapseVideo:
         dummy = Image.new("RGB", (0,0), (255,255,255))
         dd = ImageDraw.Draw(dummy)
         s = dd.textsize(text, font=fnt)
-        #print(s)
 
         # draw on canvas of measured dimensions, must have transparent background
         txt = Image.new("RGBA", s, (255,255,255,0))
@@ -248,7 +252,7 @@ class TimelapseVideo:
         # crop to actual dimensions now that we can measure those
         txt = txt.crop(txt.getbbox())
 
-        return np.array(txt)
+        return ImageClip(np.array(txt))
 
     def __draw_progress_pieslice(self, size, completion):
 
@@ -260,19 +264,23 @@ class TimelapseVideo:
         d = ImageDraw.Draw(pie)
         d.pieslice([0,0,size-1,size-1], 0, 360, fill=(255,255,255,128))
         d.pieslice([0,0,size-1,size-1], -90, completion * 360 - 90, fill=(255,255,255,255))
-        pie = pie.resize((int(size / factor), int(size / factor)))
-        return np.array(pie)
 
-    def process(self, point):
+        pie = pie.resize((int(size / factor), int(size / factor)))
+        return ImageClip(np.array(pie))
+
+    # TODO make paths absolute via os.path.realpath(__file__), maybe also use that for temp and shapefile?
+    def process(self):
+        tile = self.video.tile
+        clip = VideoFileClip(self.video.path)
+
+        width = clip.w
+        height = clip.h
+
         result_framerate = 24
         images_per_second = 3
-        margin = int(M.video_height / 30)
+        margin = int(height / 30)
         final_frame_persist = 1
-        endcard_crossfade = 0.5
-
-        clip = VideoFileClip(self.raw)
-
-        #fast_clip = concatenate_videoclips([ImageClip(frame).set_duration(2 / result_framerate) for frame in clip.iter_frames()])
+        endcard_crossfade = 0.67
 
         # process each frame separately
         frames = []
@@ -280,22 +288,22 @@ class TimelapseVideo:
             print("processing frame " + str(n+1))
             clip = ImageClip(frame)
 
-            pieslice_height = int(M.video_height / 13.5)  # manually dialled in to match font appearance
-            pieslice = ImageClip(self.__draw_progress_pieslice(pieslice_height, n / (M.frames - 1)))
-            pieslice = pieslice.set_position((clip.size[0] - pieslice.size[0] - margin, margin))
-            year = ImageClip(self.__draw_text(M.capture_times[n], int(M.video_height / 7)))
-            year = year.set_position((clip.size[0] - year.size[0] - pieslice.size[0] - margin - margin, margin))
+            pieslice_height = int(height / 13.5)  # manually dialled in to match font appearance
+            pieslice = self.__draw_progress_pieslice(pieslice_height, n / (len(self.capture_times) - 1))
+            pieslice = pieslice.set_position((width - pieslice.size[0] - margin, margin))
+            year = self.__draw_text(self.capture_times[n], int(height / 7))
+            year = year.set_position((width - year.size[0] - pieslice.size[0] - margin - margin, margin))
 
             # all of the following could be done outside the loop, which seems like it would be faster, but it's way slower
             # TODO still, define them outside?
-            geopoint = ImageClip(self.__draw_text(point.fancy(), int(M.video_height / 15)))
+            geopoint = self.__draw_text(self.geopoint.fancy(), int(height / 15))
             geopoint = geopoint.set_position((margin, margin))
-            area_w = round(self.tile.level.meters_per_pixel * M.video_width / 1000, 2)
-            area_h = round(self.tile.level.meters_per_pixel * M.video_height / 1000, 2)
-            area = ImageClip(self.__draw_text(f"{area_w} x {area_h} km", int(M.video_height / 22)))
+            area_w = round(tile.level.meters_per_pixel * width / 1000, 2)
+            area_h = round(tile.level.meters_per_pixel * height / 1000, 2)
+            area = self.__draw_text(f"{area_w} x {area_h} km", int(height / 22))
             area = area.set_position((margin, margin + geopoint.size[1] + int(0.67 * area.size[1])))
-            source = ImageClip(self.__draw_text("Source: Google Earth Timelapse (Google, Landsat, Copernicus)", int(M.video_height / 40)))
-            source = source.set_position((clip.size[0] - source.size[0] - margin, clip.size[1] - source.size[1] - margin))
+            source = self.__draw_text("Source: Google Earth Timelapse (Google, Landsat, Copernicus)", int(height / 40))
+            source = source.set_position((width - source.size[0] - margin, height - source.size[1] - margin))
 
             clip = CompositeVideoClip([clip, pieslice, year, geopoint, area, source])
             frames.append(clip)
@@ -314,41 +322,37 @@ class TimelapseVideo:
         map_scale = background.size[0] / worldmap.size[0]
         worldmap = worldmap.resize((background.size[0], map_scale * worldmap.size[1]))
         pointer = ImageClip("pointer.png").resize(map_scale)
-        pointer_x = worldmap.size[0]/2 * (1 + (point.lon / 180)) - pointer.size[0]/2
-        pointer_y = worldmap.size[1]/2 * (1 - (point.lat / 90)) - pointer.size[1]/2 # "-" since x increased from top while lat increases from bottom
+        pointer_x = worldmap.size[0]/2 * (1 + (self.geopoint.lon / 180)) - pointer.size[0]/2
+        pointer_y = worldmap.size[1]/2 * (1 - (self.geopoint.lat / 90)) - pointer.size[1]/2 # "-" since x increased from top while lat increases from bottom
         pointer = ImageClip("pointer.png").resize(map_scale).set_position((pointer_x,pointer_y))
-        geopoint = ImageClip(self.__draw_text(point.fancy(), int(1.3 * pointer.size[1])))
+        geopoint = self.__draw_text(self.geopoint.fancy(), int(1.3 * pointer.size[1]))
         geopoint_x = None
         geopoint_y = pointer_y + (pointer.size[1] - geopoint.size[1]) / 2
-        if point.lon < 0:
+        if self.geopoint.lon < 0:
             geopoint_x = pointer_x + 1.5 * pointer.size[0]
         else:
             geopoint_x = pointer_x - geopoint.size[0] - 0.5 * pointer.size[0]
         geopoint = geopoint.set_position((geopoint_x, geopoint_y))
 
-        # TODO refine this
-        url = f"{M.timemachine_repository_url}{M.dataset}/{self.tile.level.level}/{self.tile.row}/{self.tile.col}.mp4"
-        credit_small_line2 = ImageClip(self.__draw_text(url, int(M.video_height / 40)))
-        credit_small_line2 = credit_small_line2.set_position((clip.size[0]/2 - credit_small_line2.size[0]/2, clip.size[1] - credit_small_line2.size[1] - margin))
-        credit_small_line1 = ImageClip(self.__draw_text("twitter.com/earthacrosstime • bot source code: github.com/doersino/earthacrosstime • typeface: optician sans", int(M.video_height / 40)))
-        credit_small_line1 = credit_small_line1.set_position((clip.size[0]/2 - credit_small_line1.size[0]/2, clip.size[1] - credit_small_line1.size[1] - credit_small_line2.size[1] * 1.5 - margin))
-        credit = ImageClip(self.__draw_text("@earthacrosstime", int(M.video_height / 22)))
-        credit = credit.set_position((clip.size[0]/2 - credit.size[0]/2, clip.size[1] - credit.size[1] - credit_small_line1.size[1] * 1.5 - credit_small_line2.size[1] * 2 - margin))
+        credit_small_line2 = self.__draw_text(self.video.url, int(height / 40))
+        credit_small_line2 = credit_small_line2.set_position((width/2 - credit_small_line2.size[0]/2, height - credit_small_line2.size[1] - margin))
+        credit_small_line1 = self.__draw_text("twitter.com/earthacrosstime • bot source code: github.com/doersino/earthacrosstime • typeface: optician sans", int(height / 40))
+        credit_small_line1 = credit_small_line1.set_position((width/2 - credit_small_line1.size[0]/2, height - credit_small_line1.size[1] - credit_small_line2.size[1] * 1.5 - margin))
+        credit = self.__draw_text("@earthacrosstime", int(height / 22))
+        credit = credit.set_position((width/2 - credit.size[0]/2, height - credit.size[1] - credit_small_line1.size[1] * 1.5 - credit_small_line2.size[1] * 2 - margin))
 
         endcard = CompositeVideoClip([background, worldmap, pointer, geopoint, credit, credit_small_line1, credit_small_line2])
-        endcard_fade = CompositeVideoClip([final_clip.set_duration(endcard_crossfade), endcard.set_duration(endcard_crossfade).crossfadein(endcard_crossfade)])
+        endcard_fade = CompositeVideoClip([final_clip.set_duration(endcard_crossfade).fadeout(endcard_crossfade), endcard.set_duration(endcard_crossfade).crossfadein(endcard_crossfade)])
         endcard = endcard.set_duration(4)
 
         # finish!
         clip = concatenate_videoclips([clip, endcard_fade, endcard])
         clip = clip.set_fps(result_framerate)
-        clip.write_videofile(self.processed)  # logger=None
+        clip.write_videofile(self.path)  # logger=None
 
 
 
 def main():
-    global M
-
     # load configuration either from config.ini or from a user-supplied file
     # (the latter option is handy if you want to run multiple instances of
     # ærialbot with different configurations)
@@ -356,25 +360,26 @@ def main():
     if (len(sys.argv) == 2):
         config_path = sys.argv[1]
     config = ConfigObj(config_path, unrepr=True)
-    C = Config(config)
+    c = Config(config)
 
     # fetch metadata
-    M = MetadataFetcher(C.timemachine_repository_url).fetch()
+    metadata = MetadataFetcher(c.timemachine_repository_url).fetch()
 
     #geopoint = GeoPoint(46.469029, 76.040519)
-    geopoint = GeoPoint(48.781809, 9.180357)
+    geopoint = GeoPoint(37.190449, 128.478394)
     max_meters_per_pixel = 20
 
-    proj = MercatorProjection(M.projection_bounds, M.width, M.height)
+    proj = MercatorProjection(metadata.projection_bounds, metadata.width, metadata.height)
     point = proj.geopoint_to_point(geopoint)
-    level = geopoint.determine_level(M.nlevels, proj, max_meters_per_pixel)
-    tile = Tile.from_point_and_level(point, level)
+    level = geopoint.determine_level(metadata.nlevels, proj, max_meters_per_pixel)
+    tile = Tile.from_point_and_level(point, level, metadata)
     print(tile.__dict__)
-    timelapse = TimelapseVideo(tile, C.temp_dir)
-    timelapse.download()
-    timelapse.verify()
-    timelapse.process(geopoint)
-    print(timelapse.processed)
+    video = RawVideo(tile, f"{metadata.timemachine_repository_url}{metadata.dataset}", c.temp_dir)
+    video.download()
+    video.check_against(metadata)
+    editor = VideoEditor(video, geopoint, metadata.capture_times)
+    editor.process()
+    print(editor.path)
 
 if __name__ == "__main__":
 
