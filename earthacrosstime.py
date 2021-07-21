2b1134756324ad0630aba39c6484b594604a0344
Noah Doersing <git@noahdoersing.com>
2021-07-21 07:26:02 +0200

    Add note on option parsing for negative latitudes

--- earthacrosstime.py
+++ earthacrosstime.py
@@ -897,7 +897,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('config_path', metavar='CONFIG_PATH', type=str, nargs='?', default="config.ini", help='config file to use instead of looking for config.ini in the current working directory')
-    parser.add_argument('-p', '--point', dest='point', metavar='LAT,LON', type=str, help='a point, e.g. \'37.453896,126.446829\', that will override your configuration')
+    parser.add_argument('-p', '--point', dest='point', metavar='LAT,LON', type=str, help='a point, e.g. \'37.453896,126.446829\', that will override your configuration (if its latitide is negative, option parsing might throw an error – simply write -p="LAT,LON" in that case)')  # https://stackoverflow.com/questions/16174992/cant-get-argparse-to-read-quoted-string-with-dashes-in-it
     parser.add_argument('-m', '--max-meters-per-pixel', dest='max_meters_per_pixel', metavar='N', type=float, help='a maximum meters per pixel constraint that will override your configuration')
     args = parser.parse_args()
 
